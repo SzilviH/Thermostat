a9cab551d6fda75897fe4202fb999310bc2f6f89
Szilvi Horvath <szil.horvath@gmail.com>
2019-02-06 16:39:18 +0000

    down, power saving on and off, energy useage functions added

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -11,13 +11,69 @@ describe('Thermostat', function(){
 
   describe('up', function(){
     it('can increase temperature', function(){
-      (thermostat.up())
+      thermostat.up()
       expect(thermostat.getTemperature()).toEqual(21);
     });
 
     it('can increase temperature by specified amount', function(){
-      (thermostat.up(5))
+      thermostat.up(5)
       expect(thermostat.getTemperature()).toEqual(25);
+    });
+    describe("when power saving mode is on", function(){
+      it("doesn't increase temperature past 25 degrees", function(){
+        thermostat.up(10)
+        expect(thermostat.getTemperature()).toEqual(25)
+      })
+    })
+    describe("when power saving mode is off", function(){
+      it("doesn't increase temperature past 32 degrees", function(){
+        thermostat.powerSavingOff()
+        thermostat.up(15)
+        expect(thermostat.getTemperature()).toEqual(32)
+      })
+    })
+  });
+
+  describe("down", function(){
+    it("decreases the temperature", function(){
+      thermostat.down()
+      expect(thermostat.getTemperature()).toEqual(19)
+    });
+    it("decreases the temperature by a set amount", function(){
+      thermostat.down(3)
+      expect(thermostat.getTemperature()).toEqual(17)
+    });
+    it("can't be turned down below 10 degrees", function(){
+      thermostat.down(15)
+      expect(thermostat.getTemperature()).toEqual(10)
+    });
+  });
+
+  describe('reset', function(){
+    it('resets the temperature to 20 degrees', function(){
+      thermostat.up(5);
+      thermostat.reset();
+      expect(thermostat.getTemperature()).toEqual(20);
+    });
+  });
+
+  describe('energy usage', function(){
+    describe("when below 18 degrees", function(){
+      it('returns low-usage', function() {
+        thermostat.down(5);
+        expect(thermostat.energyUsage()).toEqual('low-usage');
+      });
+    });
+    describe("when below 25 degrees", function(){
+      it('returns medium-usage', function(){
+        expect(thermostat.energyUsage()).toEqual('medium-usage');
+      });
+    });
+    describe("when above 25 degrees", function(){
+      it("returns high-usage", function(){
+        thermostat.up(10);
+        expect(thermostat.energyUsage()).toEqual("high-usage");
+      });
     });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,5 +1,9 @@
 function Thermostat () {
   this.DEFAULT_TEMPERATURE = 20;
+  this.MIN_TEMP = 10;
+  this.powerSavingOffMaxTemp = 32;
+  this.powerSavingOnMaxTemp = 25;
+  this.MAX_TEMP = this.powerSavingOnMaxTemp;
   this._temperature = this.DEFAULT_TEMPERATURE;
 };
 
@@ -9,4 +13,36 @@ Thermostat.prototype.getTemperature = function () {
 
 Thermostat.prototype.up = function (n) {
   n ? this._temperature += n : this._temperature ++;
+  if (this.getTemperature() > this.MAX_TEMP) {
+    this._temperature = this.MAX_TEMP
+  };
+};
+
+Thermostat.prototype.down = function(n) {
+  n ? this._temperature -= n : this._temperature --;
+  if (this.getTemperature() < this.MIN_TEMP) {
+    this._temperature = this.MIN_TEMP
+  };
+};
+
+Thermostat.prototype.powerSavingOn = function(){
+  this.MAX_TEMP = this.powerSavingOnMaxTemp
+};
+
+Thermostat.prototype.powerSavingOff = function(){
+  this.MAX_TEMP = this.powerSavingOffMaxTemp
+};
+
+Thermostat.prototype.reset = function () {
+  this._temperature = this.DEFAULT_TEMPERATURE;
+};
+
+Thermostat.prototype.energyUsage = function () {
+  if (this.getTemperature() < 18) {
+    return 'low-usage';
+  };
+   if (this.getTemperature() < 25) {
+     return 'medium-usage';
+   }
+   return 'high-usage';
 };
